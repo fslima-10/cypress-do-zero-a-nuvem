9aa1220a72c0df688e93ce47df6eac8d31dacaa0
Fabio de Souza Lima <fslima@sp.gov.br>
2025-03-28 10:14:34 -0300

    Exercicio

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,72 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('src/index.html')
+describe('Central de Atendimento ao Cliente TAT', () => {
+  beforeEach(() =>{
+   cy.visit('src/index.html')
+  }) 
+   it('verifica o título da aplicação', () => {
+   
+    cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+    
+  })
+  it('preenche os campos obrigatórios e envia o formulário.', () => {
+    cy.get('#firstName').type('Fabio')
+    cy.get('#lastName').type('Lima')
+    cy.get('#email').type('fabio@gmail.com')
+    cy.get('#phone').type('teste').should('have.value','')
+    cy.get('#phone').type('11996969857')
+   
+    
+    cy.get('#open-text-area').type('Testando envio de mensagem', { delay: 50 });
+    cy.get('.button').click();
+    
+    cy.get('.success').should('be.visible','Mensagem enviada com sucesso.')
+  })
+  it('Telefone não preenchido', () => {
+    cy.get('#firstName').type('Fabio')
+    cy.get('#lastName').type('Lima')
+    cy.get('#email').type('fabio@gmail.com')
+    cy.get('#open-text-area').type('Testando envio de mensagem', { delay: 50 });
+    cy.get('#phone-checkbox').click()
+    cy.get('.button').click();
+
+    cy.get('.error').should('have.text','Valide os campos obrigatórios!')
+  })
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('Fabio')
+    cy.get('#lastName').type('Lima')
+    cy.get('#email').type('fabio.com')
+    cy.get('#phone').type('11996969857')
+    cy.get('#open-text-area').type('Testando envio de mensagem', { delay: 50 });
+    cy.get('.button').click();   
+    cy.get('.error').should('be.visible')
+    cy.get('.error').should('have.text','Valide os campos obrigatórios!')
+})
+  it('usando a função clear', () => {
+    cy.get('#firstName')
+    .type('Fabio')
+    .should('have.value','Fabio')
+
+    cy.get('#firstName')
+    .clear()
+    .should('have.value','')
+  }) 
+  it('envia o formuário com sucesso usando um comando customizado',() => {
+    const data = {
+      firstName: 'Fabio',
+      lastName: 'Lima',
+      email: 'fabio@gmail.com',
+      Text: 'teste.'
+    }
+    
+    cy.fillMandatoryFieldsAndSubmit(data)
+    cy.get('.success').should('be.visible')
+    cy.get('.success').should('have.text','\n      Mensagem enviada com sucesso.\n    ')
+
+  })
+  it.only('Usando o cy.contains', () => {
+    //selecionando campo do tipo combobox
+    cy.get('select').select(2)
+    cy.get('select').select('Blog')
+    cy.get('select').select('youtube')
+    cy.contains('button','Enviar')
   })
 })
